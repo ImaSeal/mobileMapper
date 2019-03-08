5dcbb791ca60a7cf8acd685f55c0d929f5196513
David Levit <dlevit9226@jh-mba158-13.local>
2019-03-08 11:40:49 -0600

    added zoom and search bar to map

--- mobileMapper/ViewController.swift
+++ mobileMapper/ViewController.swift
@@ -26,6 +26,23 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         currentLocation = locations[0]
     }
-
+    @IBAction func whenSearchButtonPressed(_ sender: Any) {
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = "Parks"
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        request.region = MKCoordinateRegion(center: currentLocation.coordinate, span: span)
+        let search = MKLocalSearch(request: request)
+        search.start { (response, error) in
+            
+        }
+    }
+    @IBAction func whenZoomButonPressed(_ sender: Any) {
+        
+    let center = currentLocation.coordinate
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: center, span: span)
+        mapView.setRegion(region, animated: true)
+    }
+    
 }
 
